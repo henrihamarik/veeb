c19649d732c09f72caaa2a9f097b5647665b30ce
henri <henri.hamarik@khk.ee>
2017-12-20 12:46:42 +0200

    test koos massiividega

--- massiivid.php
+++ massiivid.php
@@ -32,3 +32,66 @@ echo '<hr />';
 foreach ($arvud as $arv) {
     echo '<i>'.$arv.'</i><br />';
 }
+
+/*
+ * 1.
+ * Koosta funktsioon nimega looMassiiv,
+ * mis võtab parameetrina massiivi elementide
+ * arvu ja loob juhuarvudest (täisarvud)
+ * vahemikust 100-999 koosneva massiivi.
+ * Loodud massiiv tuleb tagastada
+ * põhiprogrammile ja kontrollida sisu
+ * testväljastuse abil - näiteks print_r
+ * */
+
+/*
+ * 2.
+ * Loo funktsioon nimega valjastaMassiiv,
+ * mis võtab parameetrina massiivi ja
+ * väljastab tema elemendid
+ * üheveerulise tabeli kujul
+ * Väljundi loomisel kasutada foreach
+ * tsükkel
+ * */
+
+/*
+ * 3.
+ * Loo funktsioon nimega vahetaMinMax, mis võtab
+ * parameetrina täisarvude massiivi (testimises
+ * võib kasutada looMassiiv funktsiooniga
+ * genereeritud massiiv)
+ * ja leiab kõige väiksema ja kõige suurema
+ * elemendi antud massiivis ning vahetab nende asukohad.
+ *
+ * Kontrolli kas kohad on vahetatud kasutades
+ * valjastaMassiiv funktsiooni
+*/
+
+/*
+ * 4.
+ * Loo funktsioon nimega elementideKorrutis, mis
+ * võtab parameetrina täisarvude massiivi ja leiab
+ * korrutis nendest arvudest, mis on suurem kui 0
+ * ja mille indeksid on paarisarvud. Tulemus väljastatakse
+ * antud funktsiooni abil kujul:
+ *
+ * Massiiv on 1, 0, 6, 0, 0, 3, 5
+ * Tulemus: 1 * 6 * 5 = 30
+*/
+
+/*
+ * 5.
+ * Töötluses kasutada massiiv
+ * $massiiv = array(1, 1, 1, 2, 2, 2, 2, 3).
+ * Loo funktsioon mitteDubleeri, mis võtab
+ * parameetrina massiivi ja ei väljasta dubleeritud
+ * elemendid. Ehk näidemassiivi puhul tuleb väljastada
+ * ainult 1, 2, 3 - s.t. ilma dubleerimist.
+ * NB! Lahenduses tuleb kasutada AINULT ÜKS foreach
+ * tsükli abil, samuti EI SAA kasutada
+ * grupeerimisfunktisoone ega muuta antud massiivi
+ * */
+
+function looMassiiv($elementideArv) {
+    $arvud = array();
+}
