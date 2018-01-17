01a3df52c5ab33a12a5579fec2f358c71a207ec6
henri <henri.hamarik@khk.ee>
2018-01-17 08:53:24 +0200

    valmis

---
+++ oop/vtekst.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: henri
+ * Date: 17/01/18
+ * Time: 8:53
+ */
+require_once 'tekst.php'; // lisame kasutuse klass,
+// mida laiendame
+class vtekst extends tekst
+{
+}
