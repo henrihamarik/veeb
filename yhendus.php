960321cab571a6c65864ff92bfac86928ec6edb5
henri <henri.hamarik@khk.ee>
2018-01-17 08:47:14 +0200

    valmis

--- yhendus.php
+++ yhendus.php
@@ -6,33 +6,41 @@
  * Time: 13:34
  */
 
-error_reporting('E_ALL');
 
-$yhendus = mysqli_connect('localhost','user','password','database');
-
-if (!$yhendus) {
-    echo 'Probleem andmebaasi ühendamisega<br/>';
-    echo mysqli_connect_errno($yhendus).'<br/>';
-    echo mysqli_connect_errno($yhendus).'<br/>';
+error_reporting(E_ALL);
+$yhendus = mysqli_connect('localhost', 'anna_user', 'V2#RG?TjnzMY', 'anna_test');
+if(!$yhendus){
+    echo 'Probleem andmebaasiserveri ühendamisega<br />';
+    echo mysqli_connect_error($yhendus).'<br />';
+    echo mysqli_connect_errno($yhendus).'<br />';
 } else {
-    echo 'Ühendus on loodud<br/>';
-    $sql = 'INSERT INTO kasutajad(eesnimi, perenimi, synnikuupaev)'.
-    'VALUES(\'Test\',\'Kasutaja\', \'1999-03-08\')';
-    $tulemus = mysql_query($yhendus, $sql);
-    if (!$tulemus) {
+    echo 'On loodud ühendus andmebaasiga<br />';
+    $eesnimi = 'Test';
+    $sql = 'INSERT INTO kasutajad(eesnimi, perenimi, synnikuupaev) '.
+        'VALUES(\''.$eesnimi.'\', \'Kasutaja\', \'1999-03-08\')';
+    $tulemus = mysqli_query($yhendus, $sql);
+    if(!$tulemus){
         echo 'Probleem päringuga <br />';
-        echo mysql_error($yhendus).'<br />';
-        echo mysql_errno($yhendus).'<br />';
-    } else{
-        echo '<table border="1">';
-        while($rida = mysqli_fetch_assoc($tulemus)){
-            echo '<tr>';
-            foreach ($rida as $element){
-                echo '<td>'.$element.'</td>';
+        echo mysqli_error($yhendus).'<br />';
+        echo mysqli_errno($yhendus).'<br />';
+    } else {
+        echo 'Andmed on salvestatud<br />';
+        $sql = 'SELECT * FROM kasutajad';
+        $tulemus = mysqli_query($yhendus, $sql);
+        if(!$tulemus){
+            echo 'Probleem päringuga <br />';
+            echo mysqli_error($yhendus).'<br />';
+            echo mysqli_errno($yhendus).'<br />';
+        } else{
+            echo '<table border="1">';
+            while($rida = mysqli_fetch_assoc($tulemus)){
+                echo '<tr>';
+                foreach ($rida as $element){
+                    echo '<td>'.$element.'</td>';
+                }
+                echo '</tr>';
             }
-            echo '</tr>';
+            echo '</table>';
         }
-        echo '</table>';
     }
-}
 }
