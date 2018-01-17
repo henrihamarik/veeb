405fcaa51f497dbfe3e795e17d58f05c029e48f9
henri <henri.hamarik@khk.ee>
2018-01-17 08:50:47 +0200

    valmis

--- oop/tekst.php
+++ oop/tekst.php
@@ -5,3 +5,28 @@
  * Date: 17/01/18
  * Time: 8:49
  */
+
+class tekst
+{
+    // klassi omadus(ed)
+    // klassi muutuja(d)
+    var $sone = '';
+    // klassi tegevused
+    // klassi meetodid
+    // klassi konstruktor
+    /**
+     * tekst constructor.
+     */
+    public function __construct($s = '')
+    {
+        $this->maaraTekst($s);
+    }
+    // teksti määramine
+    function maaraTekst($s){
+        $this->sone = $s;
+    }
+    // teksti väljastamine
+    function prindiTekst(){
+        echo $this->sone.'<br />';
+    }
+}
