b8d72795a6068e33e918329a514a986dfab97c93
henri <henri.hamarik@khk.ee>
2018-01-17 08:52:45 +0200

    valmis

--- oop/test.php
+++ oop/test.php
@@ -7,11 +7,21 @@
  */
 
 require_once 'tekst.php';
+require_once 'vtekst.php';
 // loome reaalse objekti tekst class abil
-$minuTekst = new tekst();
+$minuTekst = new tekst('Tere Maailm!');
 // teostame testvaade antud objektist
 echo '<pre>';
 print_r($minuTekst);
 echo '</pre>';
 // väljastame objekti sone väärtus
 $minuTekst->prindiTekst();
+echo '<hr />';
+// loome reaalse objekti vtekst klassi abil
+$punaneTekst = new vtekst('Punane tekst');
+// teostame testvaade antud objektist
+echo '<pre>';
+print_r($punaneTekst);
+echo '</pre>';
+// väljastame objekti sone väärtus
+$punaneTekst->prindiTekst();
