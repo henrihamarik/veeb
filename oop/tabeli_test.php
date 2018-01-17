c3e13b9058d0383d818c4eeff0edf47d9266dbc4
henri <henri.hamarik@khk.ee>
2018-01-17 08:51:26 +0200

    valmis

--- oop/tabeli_test.php
+++ oop/tabeli_test.php
@@ -5,3 +5,16 @@
  * Date: 17/01/18
  * Time: 8:49
  */
+
+require_once 'tabel.php';
+// loome tabel kirjelduse järgi tabeli objekt
+$minuTabel = new tabel(array('a', 'b', 'c'));
+// lisame tabeli read
+$minuTabel->lisaRida(array(1, 2, 3));
+$minuTabel->lisaRida(array(4, 5, 6));
+$minuTabel->lisaReaKirjeldusega(array('c'=>7, 'a'=>8, 'b'=>9));
+// väljastame tabel test kujul
+echo '<pre>';
+print_r($minuTabel);
+echo '</pre>';
+$minuTabel->prindiTabel();
